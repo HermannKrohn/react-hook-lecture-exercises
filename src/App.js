7c6e80b002959d0e97cff3e358befd8f29d74768
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-08-09 11:17:09 -0500

    Created a custom hook:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+function useCounter(url, initialState){
+  let [ counter, updateCounter] = useState(initialState)
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then((result) => {
+        updateCounter(result.count)
+      })
+  }, [])
+
+  function updateCounterValue(newValue){
+    updateCounter(newValue)
+    fetch(url, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({ count: newValue })
+    })
+  }
+
+  return [counter, updateCounterValue]
+}
 
 function App() {
 
-  // componentDidMount() {
-  //   fetch('http://localhost:3001/counts/1')
-  //     .then(res => res.json())
-  //     .then((result) => {
-  //       this.setState({
-  //         count: result.count
-  //       })
-  //     })
-  // }
+  let [ counter, updateCounter] = useCounter('http://localhost:3001/counts/1', 0)
 
-  let [ counter, updateCounter] = useState(0)
   return (
     <div>
       <h1 onClick={() => {
