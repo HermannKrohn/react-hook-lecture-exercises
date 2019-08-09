748c8d293ccc1e48235e4cfad976e7ca97ebfce4
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-08-09 10:10:44 -0500

    Transformed class component to functional component using React Hooks

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class App extends React.Component {
+function App() {
 
-  state = {
-    count: 0
-  }
+  // componentDidMount() {
+  //   fetch('http://localhost:3001/counts/1')
+  //     .then(res => res.json())
+  //     .then((result) => {
+  //       this.setState({
+  //         count: result.count
+  //       })
+  //     })
+  // }
 
-  componentDidMount() {
-    fetch('http://localhost:3001/counts/1')
-      .then(res => res.json())
-      .then((result) => {
-        this.setState({
-          count: result.count
-        })
-      })
-  }
-
-  render() {
-    return (
-      <div>
-        <h1 onClick={() => {
-          this.setState({ count: this.state.count + 1 })
-        }}>
-          {this.state.count}
-        </h1>
-      </div>
-    );
-  }
-
+  let [ counter, updateCounter] = useState(0)
+  return (
+    <div>
+      <h1 onClick={() => {
+        updateCounter(++counter)
+      }}>
+        {counter}
+      </h1>
+    </div>
+  );
 }
 
 export default App;
